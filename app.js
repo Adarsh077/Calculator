011f01c4a7e187a458971c845f04a5b03f27c16b
Adarsh077 <adarshco077@gmail.com>
2019-05-23 20:10:23 +0530

    Added % button and removed ( )

--- app.js
+++ app.js
@@ -7,12 +7,25 @@ buttons.forEach(button => {
 	button.addEventListener('click', () => {
 		if (value === 'C') {
 			input.value = input.value.slice(0, -1);
-		} else if (value === 'E') {
+		} else if (value === 'A') {
 			input.value = '';
 		} else if (value !== '=') {
 			input.value += value;
 		} else {
-			input.value = eval(input.value);
+				input.value = eval(input.value);
 		}
 	})
+})
+
+document.addEventListener('keypress', ele => {
+	// console.log(ele.which)
+	if (ele.which >= 42 && ele.which <= 57 && ele.which !== 44) {
+		input.value += ele.key;
+	} else if (ele.which === 13) {
+		input.value = eval(input.value);
+	} else if (ele.which === 99) {
+		input.value = input.value.slice(0, -1);
+	} else if (ele.which === 65) {
+		input.value = '';
+	}
 })
