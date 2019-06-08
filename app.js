def2ef58fd766d958cd64f683c3c27a57b90647b
Adarsh077 <adarshco077@gmail.com>
2019-06-08 18:08:12 +0530

    Added manual expression eval

--- app.js
+++ app.js
@@ -1,31 +1,84 @@
 // DOM elements
-const input = document.getElementById('input');
-const buttons = document.querySelectorAll('td button');
+const input = document.getElementById("input");
+const buttons = document.querySelectorAll("td button");
+
+// Global Variables
+let newNum = "";
+let oldNum = "";
+let opt = '';
+let clear = false;
+let result;
+
+const calculator = {
+  displayNum: num => {
+    if (clear) {
+      newNum = num;
+      clear = false;
+    } else {
+      newNum += num;
+    }
+    input.value = newNum;
+  },
+  deleteOne: () => {
+    newNum = newNum.slice(0, -1);
+    input.value = newNum;
+  },
+  clearAll: () => {
+    newNum = "";
+    oldNum = "";
+    input.value = "";
+    result = "";
+  },
+  equals: (oldN, newN, ops) => {
+    result = calculator.calculate(oldN, newN, ops);
+    result = calculator.validate(result);
+    clear = true;
+    input.value = result;
+    newNum = result;
+  },
+  displayOpt: ops => {
+		opt = ops;
+    oldNum = newNum;
+    newNum = "";
+    input.value = opt;
+  },
+  calculate: (num1, num2, ops) => {
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    switch (ops) {
+      case "+":
+        return num1 + num2;
+      case "-":
+        return num1 - num2;
+      case "/":
+        return num1 / num2;
+      case "%":
+        return num1 % num2;
+      case "*":
+        return num1 * num2;
+    }
+  },
+  validate: num =>
+    num === Infinity || isNaN(num) ? "Invalid Expression!" : num
+};
 
 buttons.forEach(button => {
-	const value = button.innerHTML;
-	button.addEventListener('click', () => {
-		if (value === 'C') {
-			input.value = input.value.slice(0, -1);
-		} else if (value === 'A') {
-			input.value = '';
-		} else if (value !== '=') {
-			input.value += value;
-		} else {
-				input.value = eval(input.value);
-		}
-	})
-})
-
-document.addEventListener('keypress', ele => {
-	// console.log(ele.which)
-	if (ele.which >= 42 && ele.which <= 57 && ele.which !== 44) {
-		input.value += ele.key;
-	} else if (ele.which === 13) {
-		input.value = eval(input.value);
-	} else if (ele.which === 99) {
-		input.value = input.value.slice(0, -1);
-	} else if (ele.which === 65) {
-		input.value = '';
-	}
-})
+  const value = button.innerHTML;
+  if (value > -1 && value < 10)
+    button.addEventListener("click", () => calculator.displayNum(value));
+  else {
+    switch (value) {
+      case "C":
+        button.addEventListener("click", () => calculator.deleteOne());
+        break;
+      case "A":
+        button.addEventListener("click", () => calculator.clearAll());
+        break;
+      case "=":
+        button.addEventListener("click", () => calculator.equals(oldNum, newNum, opt));
+        break;
+      default:
+        button.addEventListener("click", () => calculator.displayOpt(value));
+    }
+  }
+});
